a17c2dbc40f97fee2d3f2bc0e6aa50a65d0bc7ee
luccamicale <luccamicale2015@gmail.com>
2023-02-24 13:07:17 -0300

    modified user model comfirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable
+         :lockable, :trackable
 
   has_many :categories, dependent: :destroy
   has_many :payments, dependent: :destroy
 
   validates :name, presence: true
-  #   validates :email, presence: true
-  #   validates :password, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
